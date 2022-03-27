1480c3824decd38055978c57d66da7645160617a
Jayjeet1013 <80195355+Jayjeet1013@users.noreply.github.com>
2022-03-27 21:17:07 +0530

    Create Client.ts

---
+++ client/components/profile/Client.ts
@@ -0,0 +1,34 @@
+import { CeramicClient } from '@ceramicnetwork/http-client'
+
+
+const ceramic = new CeramicClient('https://your-ceramic-node.com')
+
+
+async function load(id) {
+  return await ceramic.loadStream(id)
+}
+
+
+import { DID } from 'dids'
+
+
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import { getResolver } from 'key-did-resolver'
+
+
+const API_URL = 'https://your-ceramic-node.com'
+
+const ceramic = new CeramicClient(API_URL)
+
+
+
+async function authenticateCeramic(seed) {
+ 
+  const provider = new Ed25519Provider(seed)
+ 
+  const did = new DID({ provider, resolver: getResolver() })
+  
+  await did.authenticate()
+  
+  ceramic.did = did
+}
